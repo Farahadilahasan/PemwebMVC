5b3e3d1f1f839459dbbbb10e386bb146aada1357
Farahadilahasan <124495764+Farahadilahasan@users.noreply.github.com>
2024-03-27 23:04:40 +0700

    Delete app directory

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/belajar_mvc2/public');
-
-//DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'dbecommerce');
